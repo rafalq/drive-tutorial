079d0baa82814744d2fbe546e11e6b738f728845
rafalq <kruszewskirafael@gmail.com>
2025-06-20 14:29:12 +0100

    handle folder ids via routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,11 @@ export default async function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Theo Drive
+        <h1 className="flex items-center justify-center gap-4 text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <span className="text-2xl text-gray-300">---------</span>{" "}
+          <span>Theo Drive</span>
+          <span className="text-2xl text-gray-300">---------</span>
         </h1>
-      </div>
-      <div className="w-full max-w-2/3 px-8">
-        <DriveContents folders={folders} files={files} />
       </div>
     </main>
   );
